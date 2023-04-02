a2a3317194c87312486f0500bee5d890c8fd9af4
Abdullah Al Asif <alexasif4@gmail.com>
2023-04-02 10:58:34 +0600

    isLeapYear

--- isLeapYear.js
+++ isLeapYear.js
@@ -9,17 +9,34 @@
 // }
 
 
-function isLeapYear (year) {
-    const remainder = year % 4;
-    if(remainder === 0){
-        return true
+// function isLeapYear (year) {
+//     const remainder = year % 4;
+//     if(remainder === 0){
+//         return true
+//     }
+//     return false
+// }
+
+
+// const isMyYearLeapYear = isLeapYear(1933);
+// console.log('my year:', isMyYearLeapYear)
+// const isHerYearLeapYear = isLeapYear(2020);
+// console.log('Her Year:', isHerYearLeapYear)
+
+
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
     }
-    return false
 }
 
+// take input
+const year = ('2100');
 
-const isMyYearLeapYear = isLeapYear(1933);
-console.log('my year:', isMyYearLeapYear)
-const isHerYearLeapYear = isLeapYear(2020);
-console.log('Her Year:', isHerYearLeapYear)
+checkLeapYear(year);
 
