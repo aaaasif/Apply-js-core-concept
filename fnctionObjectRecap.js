cd030a29c48499c2d678b8af0351c750f527c71a
Abdullah Al Asif <alexasif4@gmail.com>
2023-04-06 04:01:16 +0600

    Practise Day

---
+++ fnctionObjectRecap.js
@@ -0,0 +1,34 @@
+
+// #six Core Concept
+
+// 1. Varible( Declare, naming Convention, type(string, bullian, numeric) , premative, none premative, math operation)
+
+// 2. Condition (comparison: >,< ,<= ,>=,===,==, !==, if, else)
+
+// 3. Array: (Declare,index, get value, indexof,push,pop)
+
+// 4. loop: (forloop, while loop, breck, continue)
+
+// 5.fuction  (declare,properties, return,call, use, use return from a function)
+
+// 6.Object: ( declare, properties, keys, value, get pop value, set value,loop  object properties)
+
+//---------------------------------------------------------------------------------------------------------
+
+// 
+
+// 
+// 1. Google : function declaration vs function expression,
+//   arrow function,
+//     loop vs function,
+//      Array vs object,
+//       integers vs float,
+//         == vs ===,
+//          deafrance between var, let, const.
+// 
+//----------------------------------------------------------------------------------------------------------
+// 
+// 1. calculate calsius to fahrenheitRRRRRERGGERG
+// 2. interest: principal, invest rate, time= interest ammount
+// 3.complex interst
+// 
